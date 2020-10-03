f43ce689a36b2e483365236cd62b20664b68c0b4
clayton <me@clayton.cc>
2020-10-03 10:40:40 -0700

    No dot at end of message

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const visit = require('unist-util-visit');
 
 const wikiUrlRegex = /^(https?:\/\/(?:osu|new)\.ppy\.sh(?:\/help)?)?\/wiki\/(.+)/;
 const wikiUriWarnings = [
-    [/\.md$/, 'Wiki links must not include the file name.'],
-    [/\/#.*/, 'Wiki section links must not have a slash before the "#".'],
-    [/\/$/, 'Wiki links must not have a trailing slash.'],
+    [/\.md$/, 'Wiki links must not include the file name'],
+    [/\/#.*/, 'Wiki section links must not have a slash before the "#"'],
+    [/\/$/, 'Wiki links must not have a trailing slash'],
 ];
 
 function osuWikiLinks(tree, file) {
@@ -23,8 +23,8 @@ function osuWikiLinks(tree, file) {
         if (wikiUriMatch === null)
             return;
 
-        if (wikiUriMatch[1] !== null)
-            file.message('Wiki links must use /wiki/{article-name}, not the full URL.', node);
+        if (wikiUriMatch[1] !== undefined)
+            file.message('Wiki links must use /wiki/{article-name}, not the full URL', node);
 
         wikiUriWarnings.forEach(function ([wikiUriRegex, warning]) {
             if (wikiUriRegex.test(wikiUriMatch[2]))
